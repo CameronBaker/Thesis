7c162eb0939f364de3672eb5952e666b6d669ef1
Cameron Baker <cdb1516@rit.edu>
2016-01-22 16:55:57 -0500

    PyMOL script adjustment

--- ProcessProtein.py
+++ ProcessProtein.py
@@ -12,11 +12,24 @@ Cameron Baker
 '''
 
 from pymol import cmd
+from pymol import stored
 import urllib2
 import re
 
 def ActiveSites(prot):
-
+	'''
+	This function implements a command into pymol that allows the user to 
+	isolate active sites for a given protein from the pdb file as per locations
+	denoted within the catalytic site atlas. It involves the parsing of the 
+	locations from the entry for the protein within the atlas.
+	
+	If active sites are found, a new object is built within pymol containing
+	only those residues in their proper orientation
+	
+	No action if no active sites are found
+	
+	Internet connection needed
+	'''
 	cmd.fetch(prot)
 	ActiveRes = []
 	response = urllib2.urlopen('https://www.ebi.ac.uk/thornton-srv/databases/CSA/SearchResults.php?PDBID=%s'%prot)
@@ -35,16 +48,36 @@ def ActiveSites(prot):
 		ActiveRes = list(set(ActiveRes))
 		
 		#Align against the full protein if there are no active residues
-		#As long as one protein has an active site it should align well		
+		#As long as one protein has an active site it should align well	
+	print "Protein recieved"
 	if len(ActiveRes) != 0:
 		SelectStatement = "+".join(ActiveRes)
 		cmd.create('%s_active'%(prot),'resi %s in %s'%(SelectStatement,prot))
 		cmd.delete(prot)
+		print "Active sites loaded"
 	else:
 		print "no active sites found"
 		
-	reponse.close()
+	response.close()
 
 cmd.extend("ActiveSites", ActiveSites)
 print "ActiveSites command added"
 
+def SiteDistances(prot):
+	'''
+	This function implements a command within pymol that will find the 
+	centermost residue within a protein. It works by reading out all
+	of the distances between a given residue's alpha carbon and each other 
+	residue's alpha carbons. The residue with the smallest average distance 
+	across all residues will be the centermost.
+	
+	Once the centermost residue is determined, the angle between that 
+	residue's alpha carbon and each other residue's alpha and beta
+	carbons will be determined and returned to the user.
+	'''
+	stored.list = []
+	cmd.iterate("(name ca)","stored.list.append((resi,resn))")
+	print stored.list
+	
+cmd.extend("SiteDistances",SiteDistances)
+print "SiteDistances command added"
